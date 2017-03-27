fb9167ed66730ccee184ea82204484b34e52ed1d
Alfredo Ariaudo <aariaudo@grupoprominente.com>
2017-03-27 15:36:05 -0300

    Navigation drawer and dummy fragments for each menu

---
+++ app/src/main/java/com/prominente/android/vittal/fragments/QueriesFragment.java
@@ -0,0 +1,19 @@
+package com.prominente.android.vittal.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.prominente.android.vittal.R;
+
+public class QueriesFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_queries, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/prominente/android/vittal/fragments/QuotationsFragment.java
@@ -0,0 +1,19 @@
+package com.prominente.android.vittal.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.prominente.android.vittal.R;
+
+public class QuotationsFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_quotations, container, false);
+        return rootView;
+    }
+}
