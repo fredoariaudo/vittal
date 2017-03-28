9ddfcfe1d201441d0b64f495b7399b6ebb7482c6
Pablo Poza <ppoza@grupoprominente.com>
2017-03-28 16:01:52 -0300

    Fill spiner Applicant Form

--- app/src/main/java/com/prominente/android/vittal/fragments/ApplicantFormFragment.java
+++ app/src/main/java/com/prominente/android/vittal/fragments/ApplicantFormFragment.java
@@ -7,13 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 
 import com.prominente.android.vittal.R;
 
 
 public class ApplicantFormFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+
+    private String[] arraySpinner;
 
     public ApplicantFormFragment() {
         // Required empty public constructor
@@ -34,7 +38,9 @@ public class ApplicantFormFragment extends Fragment implements DatePickerDialog.
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_applicant_form, container, false);
 
-        Button pickDateButton = (Button) v.findViewById(R.id.fragment_application_form_button_pick_date);
+
+        // Pick date button.
+        Button pickDateButton = (Button) v.findViewById(R.id.fr_applicant_form_btn_pick_date);
         pickDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +48,13 @@ public class ApplicantFormFragment extends Fragment implements DatePickerDialog.
             }
         });
 
+        arraySpinner = new String[] {
+                "AP#100", "AP#200", "AP#300", "AP#400", "AP#500"
+        };
+        Spinner s = (Spinner) v.findViewById(R.id.fr_applicant_form_spn_protected_area_code);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_spinner_item, arraySpinner);
+        s.setAdapter(adapter);
 
 
         return v;
