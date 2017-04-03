93d265aff85cce4469a5d15e3afec4ea5e76ef66
Pablo Poza <ppoza@grupoprominente.com>
2017-04-03 11:48:43 -0300

    Save state Modality Form

--- app/src/main/java/com/prominente/android/vittal/fragments/ModalityFormFragment.java
+++ app/src/main/java/com/prominente/android/vittal/fragments/ModalityFormFragment.java
@@ -2,18 +2,28 @@ package com.prominente.android.vittal.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.prominente.android.vittal.R;
+import com.prominente.android.vittal.constants.SaveStateKeys;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ModalityFormFragment extends Fragment {
 
+    EditText additionalPerExcedentHelpTextView;
+    EditText aditionalPerServiceRequest;
+    EditText capitaAmountEditText;
+    EditText coseguroEditText;
+    EditText detailOtherEditText;
+    EditText hiredServiceAmountEditText;
+    EditText observationsEditText;
 
     public ModalityFormFragment() {
         // Required empty public constructor
@@ -28,7 +38,44 @@ public class ModalityFormFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modality_form, container, false);
+        View view = inflater.inflate(R.layout.fragment_modality_form, container, false);
+
+        EditText aditionalPerExcedentHelpTextView = (EditText) view.findViewById(R.id.fr_modality_form_et_additional_per_excedent_help);
+        EditText aditionalPerServiceRequest = (EditText) view.findViewById(R.id.fr_modality_form_et_aditional_per_service_request);
+        EditText capitaAmountEditText = (EditText) view.findViewById(R.id.fr_modality_form_et_capita_amount);
+        EditText coseguroEditText = (EditText) view.findViewById(R.id.fr_modality_form_et_coseguro);
+        EditText detailOtherEditText = (EditText) view.findViewById(R.id.fr_modality_form_et_detail_others);
+        EditText hiredServiceAmountEditText = (EditText) view.findViewById(R.id.fr_modality_form_et_hired_service_ammount);
+        EditText observationsEditText = (EditText) view.findViewById(R.id.fr_modality_form_et_observations);
+
+
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(SaveStateKeys.ADDITIONAL_PER_EXCEDENT_HELP , additionalPerExcedentHelpTextView.getText().toString());
+        outState.putString(SaveStateKeys.ADDITIONAL_PER_SERVICE_REQUEST ,aditionalPerServiceRequest.getText().toString());
+        outState.putString(SaveStateKeys.CAPITA_AMOUNT ,capitaAmountEditText.getText().toString());
+        outState.putString(SaveStateKeys.COSEGURO ,coseguroEditText.getText().toString());
+        outState.putString(SaveStateKeys.DETAIL_OTHERS ,detailOtherEditText.getText().toString());
+        outState.putString(SaveStateKeys.HIRED_SERVICE_AMOUNT ,hiredServiceAmountEditText.getText().toString());
+        outState.putString(SaveStateKeys.OBSERVATIONS ,observationsEditText.getText().toString());
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if(savedInstanceState != null) {
+            additionalPerExcedentHelpTextView.setText(savedInstanceState.getString(SaveStateKeys.ADDITIONAL_PER_EXCEDENT_HELP));
+            aditionalPerServiceRequest.setText(savedInstanceState.getString(SaveStateKeys.ADDITIONAL_PER_SERVICE_REQUEST));
+            capitaAmountEditText.setText(savedInstanceState.getString(SaveStateKeys.CAPITA_AMOUNT));
+            coseguroEditText.setText(savedInstanceState.getString(SaveStateKeys.COSEGURO));
+            detailOtherEditText.setText(savedInstanceState.getString(SaveStateKeys.DETAIL_OTHERS));
+            hiredServiceAmountEditText.setText(savedInstanceState.getString(SaveStateKeys.HIRED_SERVICE_AMOUNT));
+            observationsEditText.setText(savedInstanceState.getString(SaveStateKeys.OBSERVATIONS));
+        }
     }
 
 }
