324ba6b2b8c659ba0f56e6f3cde752d9e24af213
Alfredo Ariaudo <aariaudo@grupoprominente.com>
2017-04-06 17:34:04 -0300

    Partial implementation delete on Sale detail

--- app/src/main/java/com/prominente/android/vittal/constants/RequestCodes.java
+++ app/src/main/java/com/prominente/android/vittal/constants/RequestCodes.java
@@ -3,4 +3,5 @@ package com.prominente.android.vittal.constants;
 public class RequestCodes
 {
     public static final int REQUEST_NEW_SALE = 1;
+    public static final int REQUEST_MODIFY_SALE = 2;
 }
