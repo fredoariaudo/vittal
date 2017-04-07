fae15c44689c5ed67a9199584df5935e29704422
Pablo Poza <ppoza@grupoprominente.com>
2017-04-07 16:14:41 -0300

    Implements Observable in FormModel

--- app/src/main/java/com/prominente/android/vittal/model/FormModel.java
+++ app/src/main/java/com/prominente/android/vittal/model/FormModel.java
@@ -2,6 +2,9 @@ package com.prominente.android.vittal.model;
 
 
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.Observable;
+import android.databinding.PropertyChangeRegistry;
 
 import com.prominente.android.vittal.application.VittalApplication;
 import com.prominente.android.vittal.interfaces.Formeable;
@@ -12,7 +15,7 @@ import java.io.Serializable;
  * Created by Pablo Poza on 4/4/2017.
  */
 
-public abstract class FormModel extends BaseObservable implements Formeable,Serializable {
+public abstract class FormModel implements Formeable,Serializable, Observable {
 
     private static final long serialVersionUID = 7526472295622776147L;
     protected long id;
@@ -23,6 +26,58 @@ public abstract class FormModel extends BaseObservable implements Formeable,Seri
 
     public void setId(long id) {
         this.id = id;
+    }
+
+
+    // Observable
+    private transient PropertyChangeRegistry mCallbacks;
+
+    @Override
+    public void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        synchronized (this) {
+            if (mCallbacks == null) {
+                mCallbacks = new PropertyChangeRegistry();
+            }
+        }
+        mCallbacks.add(callback);
+    }
+
+    @Override
+    public void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        synchronized (this) {
+            if (mCallbacks == null) {
+                return;
+            }
+        }
+        mCallbacks.remove(callback);
+    }
+
+    /**
+     * Notifies listeners that all properties of this instance have changed.
+     */
+    public void notifyChange() {
+        synchronized (this) {
+            if (mCallbacks == null) {
+                return;
+            }
+        }
+        mCallbacks.notifyCallbacks(this, 0, null);
+    }
+
+    /**
+     * Notifies listeners that a specific property has changed. The getter for the property
+     * that changes should be marked with {@link Bindable} to generate a field in
+     * <code>BR</code> to be used as <code>fieldId</code>.
+     *
+     * @param fieldId The generated BR id for the Bindable field.
+     */
+    public void notifyPropertyChanged(int fieldId) {
+        synchronized (this) {
+            if (mCallbacks == null) {
+                return;
+            }
+        }
+        mCallbacks.notifyCallbacks(this, fieldId, null);
     }
 
 }
